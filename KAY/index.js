654805ca099ca1fd72a7fe02947d3ce7d88e355f
AvielDagan <48187820+AvielDagan@users.noreply.github.com>
2021-03-05 09:01:21 +0200

    /

---
+++ KAY/index.js
@@ -0,0 +1,10 @@
+  
+/**
+ * @project KAY
+ * @author Itay Gershman & Aviel Dagan
+ * @license Itay Gershman & Aviel Dagan
+ */
+
+require('dotenv').config();
+require('./config/db');
+require('./app');
